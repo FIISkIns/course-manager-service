4094c5fb0428158624dc7ea6f8d7e6e78e94e595
Marian Gafitescu <pmgafitescu@gmail.com>
2018-06-13 12:04:18 +0300

    Added health check and printed the errors in the log

--- service.go
+++ service.go
@@ -34,23 +34,27 @@ func HandleCourseGet(w http.ResponseWriter, r *http.Request, ps httprouter.Param
 	courseSearched := ps.ByName("course")
 	course, err := getCourse(courseSearched)
 	if err != nil {
-		http.Error(w, "Course does not exist", 404)
+		http.Error(w, "Course "+courseSearched+" does not exist", 404)
+		log.Println("Course " + courseSearched + " does not exist")
 	} else {
 		var courseInfo BaseCourseInfo
 		resp, err := http.Get(course.URL)
 		if err != nil {
-			http.Error(w, "Could not access the course", http.StatusInternalServerError)
+			http.Error(w, "Could not access the course "+courseSearched, http.StatusInternalServerError)
+			log.Println("Could not acces the course " + courseSearched)
 		} else {
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				http.Error(w, "Could not read from the course", http.StatusInternalServerError)
+				http.Error(w, "Could not read from the course "+courseSearched, http.StatusInternalServerError)
+				log.Println("Could not read from the course " + courseSearched)
 			} else {
 				json.Unmarshal(body, &courseInfo)
 				course.Name = courseInfo.Title
 
 				data, err := json.Marshal(&course)
 				if err != nil {
-					http.Error(w, "Could not serialize course", http.StatusInternalServerError)
+					http.Error(w, "Could not serialize course "+courseSearched, http.StatusInternalServerError)
+					log.Println("Could not serialize course " + courseSearched)
 				} else {
 
 					w.Header().Set("Content-Type", "application/json")
@@ -74,16 +78,18 @@ func HandleCoursePut(w http.ResponseWriter, r *http.Request, ps httprouter.Param
 	if err != nil || course.URL == "" {
 		err = insertCourse(courseInfo)
 		if err != nil {
-			http.Error(w, "Could not insert course.", http.StatusInternalServerError)
+			http.Error(w, "Could not insert course "+courseSearched, http.StatusInternalServerError)
+			log.Println("Could not insert course" + courseSearched)
 		} else {
-			fmt.Fprintf(w, "Course inserted.")
+			fmt.Fprintf(w, "Course "+courseSearched+" inserted.")
 		}
 	} else {
 		err = updateCourse(courseInfo, courseSearched)
 		if err != nil {
-			http.Error(w, "Could not update course.", http.StatusInternalServerError)
+			http.Error(w, "Could not update course "+courseSearched, http.StatusInternalServerError)
+			log.Println("Could not update course " + courseSearched)
 		} else {
-			fmt.Fprintf(w, "Course updated.")
+			fmt.Fprintf(w, "Course "+courseSearched+" updated.")
 		}
 	}
 }
@@ -103,15 +109,17 @@ func updateCourse(info CourseInfo, id string) error {
 func HandleCoursePost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Could not insert course.", http.StatusInternalServerError)
+		http.Error(w, "Could not  read the content to be inserted", http.StatusInternalServerError)
+		log.Println("Could not read the content to be inserted")
 	}
 	var courseInfo CourseInfo
 	json.Unmarshal(body, &courseInfo)
 	err = insertCourse(courseInfo)
 	if err != nil {
-		http.Error(w, "Could not insert course.", http.StatusInternalServerError)
+		http.Error(w, "Could not insert course "+courseInfo.Id, http.StatusInternalServerError)
+		log.Println("Could not insert course " + courseInfo.Id)
 	} else {
-		fmt.Fprintf(w, "Course inserted.")
+		fmt.Fprintf(w, "Course "+courseInfo.Id+" inserted.")
 	}
 
 }
@@ -133,17 +141,20 @@ func HandleCoursesFunction(w http.ResponseWriter, r *http.Request, _ httprouter.
 	worked := true
 	if err != nil || courses == nil {
 		http.Error(w, "No courses available", 404)
+		log.Println("No course available")
 	} else {
 		var courseInfo BaseCourseInfo
 		for index, course := range courses {
 			resp, err := http.Get(course.URL)
 			if err != nil {
 				http.Error(w, "Could not access "+course.Id, http.StatusInternalServerError)
+				log.Println("Could not acces " + course.Id)
 				worked = false
 			} else {
 				body, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
-					fmt.Println(err)
+					http.Error(w, "Could not read from "+course.Id, http.StatusInternalServerError)
+					log.Println("Could not read from" + course.Id)
 				}
 				json.Unmarshal(body, &courseInfo)
 				courses[index].Name = courseInfo.Title
@@ -153,6 +164,7 @@ func HandleCoursesFunction(w http.ResponseWriter, r *http.Request, _ httprouter.
 		data, err := json.Marshal(&courses)
 		if err != nil {
 			http.Error(w, "Could not serialize course list", http.StatusInternalServerError)
+			log.Println("Could not serialize course list")
 		} else if worked == true {
 			w.Header().Set("Content-Type", "application/json")
 			w.Write(data)
@@ -188,11 +200,13 @@ func HandleCourseDelete(w http.ResponseWriter, r *http.Request, ps httprouter.Pa
 
 	message, err := deleteCourse(idCourse)
 	if message == "failure" {
-		http.Error(w, "Course doesn't exist", 404)
+		http.Error(w, "Course "+idCourse+" does not exist", 404)
+		log.Println("Course " + idCourse + "does not exist")
 	} else if err != nil {
-		http.Error(w, "Course couldn't be deleted", http.StatusInternalServerError)
+		http.Error(w, "Course "+idCourse+"could not be deleted", http.StatusInternalServerError)
+		log.Println("Course " + idCourse + "could not be deleted")
 	} else {
-		fmt.Fprintf(w, "Course deleted.")
+		fmt.Fprintf(w, "Course "+idCourse+"deleted.")
 	}
 }
 
@@ -256,6 +270,48 @@ func initDatabase() error {
 	}
 	return nil
 }
+
+func checkHealth(w http.ResponseWriter, url string) bool {
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, "Failed to communicate with: "+url+"\nCause: "+err.Error(), http.StatusInternalServerError)
+		log.Println("Failed to communicate with: " + url + "\nCause: " + err.Error())
+		return false
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read response from: "+url+"\nCause: "+err.Error(), http.StatusInternalServerError)
+		log.Println("Failed to read response from: " + url + "\nCause: " + err.Error())
+		return false
+	}
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "Failed health check on: "+url+"\nResponse: "+string(body), http.StatusInternalServerError)
+		log.Println("Failed health check on: " + url + "\nResponse: " + string(body))
+		return false
+	}
+	return true
+}
+
+func healthCheckHandler(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	if err := database.Ping(); err != nil {
+		http.Error(w, "Database connection failed: "+err.Error(), http.StatusInternalServerError)
+		log.Println("Database connection failed: " + err.Error())
+		return
+	}
+
+	courses, err := getCourses("Select * from courselist")
+	if err != nil {
+		http.Error(w, "No courses found ", 404)
+		log.Println("No courses found")
+		return
+	}
+	for _, course := range courses {
+		if success := checkHealth(w, course.URL); !success {
+			return
+		}
+	}
+
+}
 func main() {
 
 	var err error
@@ -270,6 +326,7 @@ func main() {
 	defer database.Close()
 	router := httprouter.New()
 	router.GET("/courses", HandleCoursesFunction)
+	router.GET("/health", healthCheckHandler)
 	router.GET("/courses/:course", HandleCourseGet)
 	router.PUT("/courses/:course", HandleCoursePut)
 	router.POST("/courses/:course", HandleCoursePost)
